fe7e1f8a6f8a109a3ea6fd021c7c88661bfb0393
Yukihiro <Yukihiro@Yukihiro-PC>
2012-05-13 11:48:34 +0900

    pc idou

--- viprammo_client/src/viprammo/bgwork/TCPSocketReceiver.java
+++ viprammo_client/src/viprammo/bgwork/TCPSocketReceiver.java
@@ -1,15 +1,18 @@
 ﻿package viprammo.bgwork;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.nio.ByteBuffer;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.logging.Logger;
 
-import viprammo.util.GLOBAL_CONFIG;
+
+
+import viprammo.log.MyHandler;
 import viprammo.util.VipraUtil;
 
 /**
@@ -21,88 +24,95 @@ import viprammo.util.VipraUtil;
  */
 public class TCPSocketReceiver extends Thread {
 
+	/**
+	 * ソケット
+	 */
 	Socket socket;
-	InputStream is;
 	
-	//ストップフラグ　trueで受信停止する
-	boolean stopflag = false;
+	/**
+	 * 処理スレッド達
+	 */
+	InputStreamWorker inputworker;
+	OutputStreamWorker outputworker;
+	
+	/**
+	 * ロガー
+	 */
+	Logger logger = Logger.getLogger(TCPSocketReceiver.class.getName());
 	
 	public void run() {
 
-		//受信バッファ
-		byte[] buff = new byte[GLOBAL_CONFIG.CLIENT_BUFFER_SIZE];
+		//ログ画面へのHandler
+		logger.addHandler(new MyHandler());
 		
-		//処理に使用するバッファーDirectの方が高速
-		ByteBuffer buffer = ByteBuffer.allocateDirect(GLOBAL_CONFIG.CLIENT_BUFFER_SIZE * 10);
+		this.inputworker.start();
+		this.outputworker.start();
+		try {
+			this.inputworker.join();
+			this.outputworker.join();
+		} catch (InterruptedException e) {
+			logger.severe("ソケット切断");
+			e.printStackTrace();
+		}
 		
-		//初回の読み込みバイト数カウンタ
-		int read_num = 0;		
+	}
+	
+	/**
+	 * コンストラクタ（キャラの名前を与える）
+	 * @param name キャラの名前
+	 */
+	public TCPSocketReceiver(String name) {
 		
-		//データ中のバイト列数
-		int length = 0;
+		try {
+			//socket = new Socket("118.243.3.245", 10001);
+			socket = new Socket("127.0.0.1", 10001);
+			OutputStream os = socket.getOutputStream();
+			InputStream is = socket.getInputStream();
+			
+			//それぞれ受信送信用の処理スレッドを作る
+			this.inputworker = new InputStreamWorker(is);
+			this.outputworker = new OutputStreamWorker(os);
+			
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
-		//将来用識別
-		byte ident = 0;
-
-		//データ部取り出す用の配列
-		byte[] data_bu;
+	}
+	
+	/**
+	 * ソケットへの出力を処理するクラス
+	 * @author Yukihiro
+	 *
+	 */
+	private class InputStreamWorker extends Thread {
 		
-		while (!this.stopflag) {
+		DataInputStream dis;
 		
-			try {
+		public InputStreamWorker(InputStream inputstream) {
+			dis = new DataInputStream(inputstream);
+		}
+		
+		public void run() {
+			
+			int length = 0;
+			byte ident = 0;
+			
+			while (true) {
 				
-				read_num = is.read(buff);
-				System.out.println("readnum=" + read_num);
-				
-				buffer.put(buff, 0, read_num);
-				buffer.flip();
-				
-				//取得しなければならないlengthが0（つまりまだヘッダー読んでない）の状態で
-				//なおかつbufferのリミットが5バイトより少ない（つまりヘッダーが無い）状態であればさらにputが必要なのでcontinue
-				//ここでは断片化で受信した4byteとかがあるかもしれない
-				if ((length == 0) && (buffer.limit() < 5)) {
-					continue;
+				try {
+					length = dis.readInt();
+					ident = dis.readByte();
+					logger.finest(String.format("ヘッダー読み込み length=%d, ident=%d", length, ident));
+					
+					byte[] buff = new byte[length];
+					dis.readFully(buff);
+				} catch (IOException e) {
+					logger.severe("ソケット処理でIOエラー");
+					e.printStackTrace();
 				}
-				
-				//ヘッダーを読んで居ない状態でヘッダー分以上のデータがあればヘッダーを読む
-				if ((length == 0) && (buffer.limit() >= 5)) {
-					
-					//バッファーを読むときはflip(ここでpos0limitはputしたとこまでに)
-					//buffer.flip();
-					
-					length = buffer.getInt();
-					ident = buffer.get();
-
-				}
-
-				
-				//-------------------- ここに来る時点で必ずlengthは入ってる ----------------------------
-
-				
-				//バッファーのリミットがlength以上ならデータを読み込んで処理に回す
-				if (buffer.remaining() >= length) {
-					
-					System.out.println("remai="+buffer.remaining());
-					
-					//データ部用の配列作る
-					data_bu = new byte[length];
-					//データ取得
-					buffer.get(data_bu);
-
-					System.out.printf("length=%d, ident=%d, data=%d\n", length, ident, data_bu.length);
-					
-					//データ取得が終了したら追記できるようにしておく
-					//buffer.compact();
-					//lengthに0入れる
-					length = 0;
-					System.out.println(buffer.limit());
-					
-				}
-				
-				buffer.compact();
-				
-			} catch (IOException e) {
-				e.printStackTrace();
 			}
 			
 		}
@@ -110,69 +120,20 @@ public class TCPSocketReceiver extends Thread {
 	}
 	
 	/**
-	 * 旧受信処理
+	 * ソケットへの入力を処理するクラス
+	 * @author Yukihiro
+	 *
 	 */
-	private void rcv() {
+	private class OutputStreamWorker extends Thread {
 		
-		byte[] buff = new byte[1];
-		ArrayList<Byte> al = new ArrayList<Byte>();
+		DataOutputStream dos;
 		
-		int rnum = 0;
-		byte mae = 0;
-		try {
-			while (true) {
-				
-				rnum = is.read(buff);
-				if (rnum == -1) {
-					break;
-				}
-				
-				System.out.println(buff[0]);
-				
-				if (buff[0] == 10) {
-					if (mae == 13) {
-						al.add(buff[0]);
-						//改行受信でコマンドデリミター 
-						CharacterDrawer.getInstance().draw(new String(VipraUtil.CollectionsByteToPbyte(al), "UTF-8"));
-						al.clear();
-						mae = 0;
-					} else {
-						al.add(buff[0]);
-						mae = 0;
-					}
-				} else if (buff[0] == 13) {
-					mae = 13;
-					al.add(buff[0]);
-				} else {
-					mae = 0;
-					al.add(buff[0]);
-				}
-
-
-				
-			}
-		} catch (IOException e) {
-
-			e.printStackTrace();
+		public OutputStreamWorker(OutputStream os) {
+			dos = new DataOutputStream(os);
 		}
-		System.out.println("kireta");
-	}
-	
-	public TCPSocketReceiver(String name) {
 		
-		try {
-			//socket = new Socket("118.243.3.245", 10001);
-			socket = new Socket("127.0.0.1", 10001);
-			OutputStream os = socket.getOutputStream();
-			os.write(new String("IAM"+name+"\r\n").getBytes());
-			os.flush();
-
-			is = socket.getInputStream();
+		public void run() {
 			
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
 		
 	}
